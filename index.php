39213fbb9c9dd7207fea460e2e2818c003a0ae45
jester911 <jakeboi11@gmail.com>
2015-05-18 17:20:57 -0400

    Create index.php

---
+++ index.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html>
+
+<title>SHITTY FILE HOSTING</title>
+<head>
+	<link rel='stylesheet' type='text/css' href='home.css'>
+</head>
+
+<body>
+<h1>Uploader thingy</h1>
+
+<form action="upload.php" method="post" enctype="multipart/form-data">
+    Shit to Upload:
+    <input type="file" name="fileToUpload" id="fileToUpload">
+    <input type="submit" value="Upload Some Shit" id="s" name="submit">
+</form><br>
+<form method="LINK" action="http://poopenis.tk/uploads/">
+<input value="Uploads" type="submit" id="u">
+</form>
+<iframe src="http://poopenis.tk/uploads/count.php"></frame>
+</body>
+
+</html>
